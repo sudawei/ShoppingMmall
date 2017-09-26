cb0925723d076f45f6195b26013e42665862c1b1
imooc <1527940075@qq.com>
2017-09-26 09:24:05 +0800

    bug2

--- src/main/java/com/mmall/controller/portal/CartController.java
+++ src/main/java/com/mmall/controller/portal/CartController.java
@@ -56,72 +56,82 @@ public class CartController {
      * @param productId
      * @return
      */
+//    @RequestMapping("add.do")
+//    @ResponseBody
+//    public ServerResponse add(
+//            HttpServletRequest request, HttpServletResponse response,
+//            HttpSession session, Integer count, Integer productId )
+//            throws  IOException {
+//
+//       //将对象转换成json字符串/json字符串转成对象
+//        ObjectMapper om = new ObjectMapper();
+//        om.setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
+//        Cart cookieCart = null;
+//        //获取cookie中的购物车
+//        Cookie[] cookies = request.getCookies();
+//        if(cookies != null && cookies.length > 0){
+//            for(Cookie cookie : cookies){
+//                if((Const.CURRENT_COOKIES+"_"+productId.toString()).equals(cookie.getName())){
+//                    cookieCart = om.readValue(cookie.getValue(),Cart.class);
+//                    break;
+//                }
+//            }
+//        }
+//
+//        //cookie中的没有购物车
+//        if(cookieCart == null){
+//            //将当前商品组装成cart对象
+//            if(count == null || productId == null){
+//                return  ServerResponse.createByErrorCodeMessage(ResponseCode.ILLEAGAL_ARGUMENT.getCode(),ResponseCode.ILLEAGAL_ARGUMENT.getDesc());
+//            }
+//            cookieCart = new Cart();
+//            cookieCart.setQuantity(count);
+//            cookieCart.setProductId(productId);
+//        }else{  //此时已经存在该商品的cookie,则需要做商品数量相加
+//            cookieCart.setQuantity(count + cookieCart.getQuantity());
+//        }
+//
+//        User user =(User) session.getAttribute(Const.CURRENT_USER);
+//        if(user != null){
+//            //先将cookie中的所有的购物车添加至数据库
+//            for(Cookie cookie : cookies){
+//                if(cookie.getName().matches(Const.CURRENT_COOKIES +"_"+ "\\d+")){
+//                    cookieCart = om.readValue(cookie.getValue(),Cart.class);
+//                    iCartService.add(user.getId(),cookieCart.getQuantity(),cookieCart.getProductId());
+//                    //清空Cookie 设置存活时间为0, 立马销毁
+//                    cookie.setPath("/");
+//                    cookie.setMaxAge(-0);
+//                    response.addCookie(cookie);
+//                }
+//            }
+//            //添加本次的新建购物车
+//            iCartService.add(user.getId(),count,productId);
+//            return ServerResponse.createBySuccess("购物车成功添加到数据库中！");
+//        }else{
+//            //未登录,将保存购物车到Cookie中
+//            Writer w = new StringWriter();
+//            om.writeValue(w,cookieCart);
+//            Cookie cookie = new Cookie(Const.CURRENT_COOKIES+"_"+productId.toString(),w.toString());
+//            //设置path是可以共享cookie
+//            cookie.setPath("/");
+//            //设置Cookie过期时间: -1 表示关闭浏览器失效  0: 立即失效  >0: 单位是秒, 多少秒后失效
+//            cookie.setMaxAge(24*60*60);
+//            //Cookie写回浏览器
+//            response.addCookie(cookie);
+//            return ServerResponse.createBySuccess("购物车成功添加到cookie中",cookie);
+//        }
+//
+//
+//    }
+
     @RequestMapping("add.do")
     @ResponseBody
-    public ServerResponse add(
-            HttpServletRequest request, HttpServletResponse response,
-            HttpSession session, Integer count, Integer productId )
-            throws  IOException {
-
-       //将对象转换成json字符串/json字符串转成对象
-        ObjectMapper om = new ObjectMapper();
-        om.setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
-        Cart cookieCart = null;
-        //获取cookie中的购物车
-        Cookie[] cookies = request.getCookies();
-        if(cookies != null && cookies.length > 0){
-            for(Cookie cookie : cookies){
-                if((Const.CURRENT_COOKIES+"_"+productId.toString()).equals(cookie.getName())){
-                    cookieCart = om.readValue(cookie.getValue(),Cart.class);
-                    break;
-                }
-            }
+    public ServerResponse<CartVo> add(HttpSession session, Integer count, Integer productId){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
         }
-
-        //cookie中的没有购物车
-        if(cookieCart == null){
-            //将当前商品组装成cart对象
-            if(count == null || productId == null){
-                return  ServerResponse.createByErrorCodeMessage(ResponseCode.ILLEAGAL_ARGUMENT.getCode(),ResponseCode.ILLEAGAL_ARGUMENT.getDesc());
-            }
-            cookieCart = new Cart();
-            cookieCart.setQuantity(count);
-            cookieCart.setProductId(productId);
-        }else{  //此时已经存在该商品的cookie,则需要做商品数量相加
-            cookieCart.setQuantity(count + cookieCart.getQuantity());
-        }
-
-        User user =(User) session.getAttribute(Const.CURRENT_USER);
-        if(user != null){
-            //先将cookie中的所有的购物车添加至数据库
-            for(Cookie cookie : cookies){
-                if(cookie.getName().matches(Const.CURRENT_COOKIES +"_"+ "\\d+")){
-                    cookieCart = om.readValue(cookie.getValue(),Cart.class);
-                    iCartService.add(user.getId(),cookieCart.getQuantity(),cookieCart.getProductId());
-                    //清空Cookie 设置存活时间为0, 立马销毁
-                    cookie.setPath("/");
-                    cookie.setMaxAge(-0);
-                    response.addCookie(cookie);
-                }
-            }
-            //添加本次的新建购物车
-            iCartService.add(user.getId(),count,productId);
-            return ServerResponse.createBySuccess("购物车成功添加到数据库中！");
-        }else{
-            //未登录,将保存购物车到Cookie中
-            Writer w = new StringWriter();
-            om.writeValue(w,cookieCart);
-            Cookie cookie = new Cookie(Const.CURRENT_COOKIES+"_"+productId.toString(),w.toString());
-            //设置path是可以共享cookie
-            cookie.setPath("/");
-            //设置Cookie过期时间: -1 表示关闭浏览器失效  0: 立即失效  >0: 单位是秒, 多少秒后失效
-            cookie.setMaxAge(24*60*60);
-            //Cookie写回浏览器
-            response.addCookie(cookie);
-            return ServerResponse.createBySuccess("购物车成功添加到cookie中",cookie);
-        }
-
-
+        return iCartService.add(user.getId(),count,productId);
     }
 
     /**
